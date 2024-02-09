84c1b0df7c539eca56f14917548ddffc66f6b548
ka1412 <150510726+ka1412@users.noreply.github.com>
2024-02-09 15:49:38 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -13,8 +13,8 @@ provider "aws" {
 
 
 resource "aws_instance" "my-ec2-vm_ubuntu" {
-  ami                    = "ami-03f4878755434977f"
-  instance_type          = "t3.medium"
+  ami                    = "ami-0a7cf821b91bcccbc"
+  instance_type          = "t2.micro"
   count                  = 2
   tags = {
     "Name" = "ci-cd"
